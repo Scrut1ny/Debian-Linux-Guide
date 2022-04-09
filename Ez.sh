64e91f31b991a4dc3ee75e1967edcdae5c2ec797
0x00 <53458032+Scrut1ny@users.noreply.github.com>
2022-04-08 21:48:31 -0400

    Update Ez.sh

--- Ez.sh
+++ Ez.sh
@@ -8,24 +8,24 @@ echo
 echo Updating / Upgrading...
 echo
 
-sudo apt update -y
-sudo apt full-upgrade -y
-sudo apt --with-new-pkgs upgrade -y
-sudo apt dist-upgrade -y
+sudo apt update -y > /dev/null
+sudo apt full-upgrade -y > /dev/null
+sudo apt --with-new-pkgs upgrade -y > /dev/null
+sudo apt dist-upgrade -y > /dev/null
 
 echo
 echo Cleaning...
 echo
 
-sudo apt autoremove -y
-sudo apt autoclean -y
-sudo apt clean -y
+sudo apt autoremove -y > /dev/null
+sudo apt autoclean -y > /dev/null
+sudo apt clean -y > /dev/null
 
 echo
 echo Extra...
 echo
 
-sudo apt list --upgradable -a
+sudo apt list --upgradable -a > /dev/null
 echo
 echo Type sudo apt install package
 echo
